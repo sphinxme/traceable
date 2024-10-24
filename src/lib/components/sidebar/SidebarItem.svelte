67ec4706e446f5d8dbdffe15f891bb6246836634
sphinx <zhumingyua@hotmail.com>
2024-10-24 17:49:11 +0800

    feat:增加侧边栏

---
+++ src/lib/components/sidebar/SidebarItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let path: string;
+	export let href: string;
+
+	import { page } from '$app/stores';
+
+	// 使用导入的 page store 来获取当前路径
+	import { derived } from 'svelte/store';
+	const isActive = derived(page, ($page) => $page.url.pathname === path);
+</script>
+
+<a
+	{href}
+	class="m-1 rounded-lg p-2 px-4 text-center text-lg transition-colors duration-300 hover:bg-slate-200"
+	class:bg-slate-200={$isActive}
+>
+	<slot />
+</a>
