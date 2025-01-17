d151ab6203ec460397c543cf8d10533c5c450380
sphinx <zhumingyua@hotmail.com>
2025-01-17 17:53:20 +0800

    fix: optimize panel resizer

--- src/lib/components/ui/resizable/pane-resizer.svelte
+++ src/lib/components/ui/resizable/pane-resizer.svelte
@@ -2,9 +2,7 @@
     import { PaneResizer } from "paneforge";
 </script>
 
-<PaneResizer class="flex-col flex h-full justify-center group">
-    <div
-        style="width:3px;"
-        class="bg-slate-400 rounded-full opacity-40 mx-0.5 my-8 h-12 group-hover:h-full ease-in-out duration-300 group-hover:opacity-100 transition-all group-active:my-16 group-active:bg-slate-400"
-    ></div>
-</PaneResizer>
+<PaneResizer
+    class="h-full bg-slate-200 transition hover:bg-slate-300 mx-1 mb-4"
+    style="width:1px;"
+/>
